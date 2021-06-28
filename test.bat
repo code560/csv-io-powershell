78dd1a73856f5af6155637a486f527c352cdad6f
code560 <smartphone.560+git@gmail.com>
2021-06-28 23:30:29 +0900

    split line

--- test.bat
+++ test.bat
@@ -6,20 +6,27 @@ set LOG=log.txt
 
 set IN_FILE=foo.csv
 set TMP_FILE=tmp.dat
-set OUT_FILE=bar.csv
+set OUT_FILE=out\bar
 
 del TMP_FILE >nul 2>&1
 del OUT_FILE >nul 2>&1
-
+md out >nul 2>&1
 
 @REM set .ps="Write-Host 'Hello World';"
 
-set .ps="Import-Csv -Encoding Default %IN_FILE% | Export-Csv -NoTypeInformation -Delimiter ',' -Encoding Default -Path %TMP_FILE%;"
+set .ps="Import-Csv -Encoding Default %IN_FILE% | Export-Csv -NoTypeInformation -Delimiter ',' -Encoding Default -Path %TMP_FILE%1;"
 @REM set .ps="$csv=Import-Csv -Encoding Default foo.csv; $csv | Export-Csv -NoTypeInformation -Delimiter ',' -Encoding Default -Path bar.csv;"
 call :fnPS %.ps% > %LOG% 2>&1
 
 
-set .ps="Get-Content %TMP_FILE% | foreach { $_ -replace '""', '' } | Out-File -Encoding Default -FilePath %OUT_FILE%;"
+set .ps="Get-Content %TMP_FILE%1| foreach {$_ -replace '""', ''} | Out-File -Encoding Default -FilePath %TMP_FILE%2;"
+call :fnPS %.ps% >> %LOG% 2>&1
+
+
+@REM set .ps="$i=0; Get-Content %TMP_FILE%2 -ReadCount 1 | foreach {$_ | Out-File -Encoding Default -FilePath %OUT_FILE%_$i.csv; $i++;}"
+
+set .ps="$i=0; $h=(Get-Content %TMP_FILE%2)[0]; Get-Content %TMP_FILE%2 -ReadCount 1 | select -skip 1 | foreach {Out-File -InputObject $h -Encoding Default -FilePath %OUT_FILE%_$i.csv; $_ | Out-File -Encoding Default -FilePath %OUT_FILE%_$i.csv -Append; $i++;}"
+
 call :fnPS %.ps% >> %LOG% 2>&1
 
 popd
